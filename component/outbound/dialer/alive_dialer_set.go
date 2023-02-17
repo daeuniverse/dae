da950ea54e85bcd805e86157322ca3253c95d7ee
mzz2017 <2017@duck.com>
2023-02-18 00:34:24 +0800

    chore: upgrade softwind to support SSR UDP

--- component/outbound/dialer/alive_dialer_set.go
+++ component/outbound/dialer/alive_dialer_set.go
@@ -126,20 +126,22 @@ func (a *AliveDialerSet) NotifyLatencyChange(dialer *Dialer, alive bool) {
 		if index >= 0 {
 			// This dialer is already alive.
 		} else {
-			logrus.WithFields(logrus.Fields{
-				"dialer": dialer.Name(),
-			}).Warnln("NOT ALIVE -> ALIVE:")
 			// Dialer: not alive -> alive.
+			a.log.WithFields(logrus.Fields{
+				"dialer":  dialer.Name(),
+				"network": a.CheckTyp.StringWithoutDns(),
+			}).Infoln("NOT ALIVE -> ALIVE:")
 			a.dialerToIndex[dialer] = len(a.inorderedAliveDialerSet)
 			a.inorderedAliveDialerSet = append(a.inorderedAliveDialerSet, dialer)
 		}
 	} else {
 		index := a.dialerToIndex[dialer]
 		if index >= 0 {
-			logrus.WithFields(logrus.Fields{
-				"dialer": dialer.Name(),
-			}).Warnln("ALIVE -> NOT ALIVE:")
 			// Dialer: alive -> not alive.
+			a.log.WithFields(logrus.Fields{
+				"dialer":  dialer.Name(),
+				"network": a.CheckTyp.StringWithoutDns(),
+			}).Infoln("ALIVE -> NOT ALIVE:")
 			// Remove the dialer from inorderedAliveDialerSet.
 			if index >= len(a.inorderedAliveDialerSet) {
 				a.log.Panicf("index:%v >= len(a.inorderedAliveDialerSet):%v", index, len(a.inorderedAliveDialerSet))
