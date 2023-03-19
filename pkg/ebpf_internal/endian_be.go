eed2fa4f8f8b6f50b308680f00b11c8c7b18efa2
mzz2017 <2017@duck.com>
2023-03-19 21:50:34 +0800

    chore: support loong64 arch

--- pkg/ebpf_internal/endian_be.go
+++ pkg/ebpf_internal/endian_be.go
@@ -1,7 +1,4 @@
 //go:build armbe || arm64be || mips || mips64 || mips64p32 || ppc64 || s390 || s390x || sparc || sparc64
-// +build armbe arm64be mips mips64 mips64p32 ppc64 s390 s390x sparc sparc64
-
-// Copied from https://github.com/cilium/ebpf/blob/v0.10.0/internal/endian_be.go
 
 package internal
 
