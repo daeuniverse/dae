daeuniverse/dae

commit ee09ae17e31d47fd4fd680d5308f64368e3b13a4

feat: support iptables tproxy (#80)

component/outbound/dialer/connectivity_check.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/daeuniverse/dae/common"
 	"net"
 	"net/http"
 	"net/netip"
@@ -121,7 +122,7 @@ type TcpCheckOption struct {
 	Method string
 }
 
-func ParseTcpCheckOption(ctx context.Context, rawURL []string, method string) (opt *TcpCheckOption, err error) {
+func ParseTcpCheckOption(ctx context.Context, rawURL []string, method string, resolverNetwork string) (opt *TcpCheckOption, err error) {
 	if method == "" {
 		method = http.MethodGet
 	}
@@ -146,7 +147,7 @@ func ParseTcpCheckOption(ctx context.Context, rawURL []string, method string) (o
 	if len(rawURL) > 1 {
 		ip46 = parseIp46FromList(rawURL[1:])
 	} else {
-		ip46, err = netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, u.Hostname(), false, false)
+		ip46, err = netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, u.Hostname(), resolverNetwork, false)
 		if err != nil {
 			return nil, err
 		}
@@ -164,7 +165,7 @@ type CheckDnsOption struct {
 	*netutils.Ip46
 }
 
-func ParseCheckDnsOption(ctx context.Context, dnsHostPort []string) (opt *CheckDnsOption, err error) {
+func ParseCheckDnsOption(ctx context.Context, dnsHostPort []string, resolverNetwork string) (opt *CheckDnsOption, err error) {
 	systemDns, err := netutils.SystemDns()
 	if err != nil {
 		return nil, err
@@ -191,7 +192,7 @@ func ParseCheckDnsOption(ctx context.Context, dnsHostPort []string) (opt *CheckD
 	if len(dnsHostPort) > 1 {
 		ip46 = parseIp46FromList(dnsHostPort[1:])
 	} else {
-		ip46, err = netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, host, false, false)
+		ip46, err = netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, host, resolverNetwork, false)
 		if err != nil {
 			return nil, err
 		}
@@ -204,11 +205,12 @@ func ParseCheckDnsOption(ctx context.Context, dnsHostPort []string) (opt *CheckD
 }
 
 type TcpCheckOptionRaw struct {
-	opt    *TcpCheckOption
-	mu     sync.Mutex
-	Log    *logrus.Logger
-	Raw    []string
-	Method string
+	opt             *TcpCheckOption
+	mu              sync.Mutex
+	Log             *logrus.Logger
+	Raw             []string
+	ResolverNetwork string
+	Method          string
 }
 
 func (c *TcpCheckOptionRaw) Option() (opt *TcpCheckOption, err error) {
@@ -218,7 +220,7 @@ func (c *TcpCheckOptionRaw) Option() (opt *TcpCheckOption, err error) {
 		ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
 		defer cancel()
 		ctx = context.WithValue(ctx, "logger", c.Log)
-		tcpCheckOption, err := ParseTcpCheckOption(ctx, c.Raw, c.Method)
+		tcpCheckOption, err := ParseTcpCheckOption(ctx, c.Raw, c.Method, c.ResolverNetwork)
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse tcp_check_url: %w", err)
 		}
@@ -228,9 +230,10 @@ func (c *TcpCheckOptionRaw) Option() (opt *TcpCheckOption, err error) {
 }
 
 type CheckDnsOptionRaw struct {
-	opt *CheckDnsOption
-	mu  sync.Mutex
-	Raw []string
+	opt             *CheckDnsOption
+	mu              sync.Mutex
+	Raw             []string
+	ResolverNetwork string
 }
 
 func (c *CheckDnsOptionRaw) Option() (opt *CheckDnsOption, err error) {
@@ -239,7 +242,7 @@ func (c *CheckDnsOptionRaw) Option() (opt *CheckDnsOption, err error) {
 	if c.opt == nil {
 		ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
 		defer cancel()
-		udpCheckOption, err := ParseCheckDnsOption(ctx, c.Raw)
+		udpCheckOption, err := ParseCheckDnsOption(ctx, c.Raw, c.ResolverNetwork)
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse tcp_check_url: %w", err)
 		}
@@ -266,6 +269,10 @@ func (d *Dialer) ActivateCheck() {
 func (d *Dialer) aliveBackground() {
 	timeout := 10 * time.Second
 	cycle := d.CheckInterval
+	var tcpSomark uint32
+	if network, err := netproxy.ParseMagicNetwork(d.TcpCheckOptionRaw.ResolverNetwork); err == nil {
+		tcpSomark = network.Mark
+	}
 	tcp4CheckOpt := &CheckOption{
 		networkType: &NetworkType{
 			L4Proto:   consts.L4ProtoStr_TCP,
@@ -285,7 +292,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.HttpCheck(ctx, opt.Url, opt.Ip4, opt.Method)
+			return d.HttpCheck(ctx, opt.Url, opt.Ip4, opt.Method, tcpSomark)
 		},
 	}
 	tcp6CheckOpt := &CheckOption{
@@ -307,7 +314,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.HttpCheck(ctx, opt.Url, opt.Ip6, opt.Method)
+			return d.HttpCheck(ctx, opt.Url, opt.Ip6, opt.Method, tcpSomark)
 		},
 	}
 	tcp4CheckDnsOpt := &CheckOption{
@@ -329,7 +336,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), true)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
 		},
 	}
 	tcp6CheckDnsOpt := &CheckOption{
@@ -351,7 +358,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), true)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
 		},
 	}
 	udp4CheckDnsOpt := &CheckOption{
@@ -372,7 +379,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), false)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
 		},
 	}
 	udp6CheckDnsOpt := &CheckOption{
@@ -393,7 +400,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), false)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
 		},
 	}
 	var CheckOpts = []*CheckOption{
@@ -535,7 +542,7 @@ func (d *Dialer) Check(timeout time.Duration,
 	return ok, err
 }
 
-func (d *Dialer) HttpCheck(ctx context.Context, u *netutils.URL, ip netip.Addr, method string) (ok bool, err error) {
+func (d *Dialer) HttpCheck(ctx context.Context, u *netutils.URL, ip netip.Addr, method string, soMark uint32) (ok bool, err error) {
 	// HTTP(S) check.
 	if method == "" {
 		method = http.MethodGet
@@ -545,7 +552,7 @@ func (d *Dialer) HttpCheck(ctx context.Context, u *netutils.URL, ip netip.Addr, 
 		Transport: &http.Transport{
 			DialContext: func(ctx context.Context, network, addr string) (c net.Conn, err error) {
 				// Force to dial "ip".
-				conn, err := cd.DialTcpContext(ctx, net.JoinHostPort(ip.String(), u.Port()))
+				conn, err := cd.DialContext(ctx, common.MagicNetwork("tcp", soMark), net.JoinHostPort(ip.String(), u.Port()))
 				if err != nil {
 					return nil, err
 				}
@@ -584,8 +591,8 @@ func (d *Dialer) HttpCheck(ctx context.Context, u *netutils.URL, ip netip.Addr, 
 	}
 }
 
-func (d *Dialer) DnsCheck(ctx context.Context, dns netip.AddrPort, tcp bool) (ok bool, err error) {
-	addrs, err := netutils.ResolveNetip(ctx, d, dns, consts.UdpCheckLookupHost, dnsmessage.TypeA, tcp)
+func (d *Dialer) DnsCheck(ctx context.Context, dns netip.AddrPort, network string) (ok bool, err error) {
+	addrs, err := netutils.ResolveNetip(ctx, d, dns, consts.UdpCheckLookupHost, dnsmessage.TypeA, network)
 	if err != nil {
 		return false, err
 	}

control/control_plane.go
@@ -67,7 +67,9 @@ type ControlPlane struct {
 	wanInterface []string
 	lanInterface []string
 
-	sniffingTimeout time.Duration
+	sniffingTimeout   time.Duration
+	tproxyPortProtect bool
+	soMarkFromDae     uint32
 }
 
 func NewControlPlane(
@@ -226,9 +228,17 @@ func NewControlPlane(
 		log.Warnln("AllowInsecure is enabled, but it is not recommended. Please make sure you have to turn it on.")
 	}
 	option := &dialer.GlobalOption{
-		Log:               log,
-		TcpCheckOptionRaw: dialer.TcpCheckOptionRaw{Raw: global.TcpCheckUrl, Log: log, Method: global.TcpCheckHttpMethod},
-		CheckDnsOptionRaw: dialer.CheckDnsOptionRaw{Raw: global.UdpCheckDns},
+		Log: log,
+		TcpCheckOptionRaw: dialer.TcpCheckOptionRaw{
+			Raw:             global.TcpCheckUrl,
+			Log:             log,
+			ResolverNetwork: common.MagicNetwork("udp", global.SoMarkFromDae),
+			Method:          global.TcpCheckHttpMethod,
+		},
+		CheckDnsOptionRaw: dialer.CheckDnsOptionRaw{
+			Raw:             global.UdpCheckDns,
+			ResolverNetwork: common.MagicNetwork("udp", global.SoMarkFromDae),
+		},
 		CheckInterval:     global.CheckInterval,
 		CheckTolerance:    global.CheckTolerance,
 		CheckDnsTcp:       true,
@@ -337,23 +347,25 @@ func NewControlPlane(
 
 	ctx, cancel := context.WithCancel(context.Background())
 	plane := &ControlPlane{
-		log:              log,
-		core:             core,
-		deferFuncs:       deferFuncs,
-		listenIp:         "0.0.0.0",
-		outbounds:        outbounds,
-		dnsController:    nil,
-		onceNetworkReady: sync.Once{},
-		dialMode:         dialMode,
-		routingMatcher:   routingMatcher,
-		ctx:              ctx,
-		cancel:           cancel,
-		ready:            make(chan struct{}),
-		muRealDomainSet:  sync.Mutex{},
-		realDomainSet:    bloom.NewWithEstimates(2048, 0.001),
-		lanInterface:     global.LanInterface,
-		wanInterface:     global.WanInterface,
-		sniffingTimeout:  sniffingTimeout,
+		log:               log,
+		core:              core,
+		deferFuncs:        deferFuncs,
+		listenIp:          "0.0.0.0",
+		outbounds:         outbounds,
+		dnsController:     nil,
+		onceNetworkReady:  sync.Once{},
+		dialMode:          dialMode,
+		routingMatcher:    routingMatcher,
+		ctx:               ctx,
+		cancel:            cancel,
+		ready:             make(chan struct{}),
+		muRealDomainSet:   sync.Mutex{},
+		realDomainSet:     bloom.NewWithEstimates(2048, 0.001),
+		lanInterface:      global.LanInterface,
+		wanInterface:      global.WanInterface,
+		sniffingTimeout:   sniffingTimeout,
+		tproxyPortProtect: global.TproxyPortProtect,
+		soMarkFromDae:     global.SoMarkFromDae,
 	}
 	defer func() {
 		if err != nil {
@@ -363,9 +375,10 @@ func NewControlPlane(
 
 	/// DNS upstream.
 	dnsUpstream, err := dns.New(dnsConfig, &dns.NewOption{
-		Logger:                log,
-		LocationFinder:        locationFinder,
-		UpstreamReadyCallback: plane.dnsUpstreamReadyCallback,
+		Logger:                  log,
+		LocationFinder:          locationFinder,
+		UpstreamReadyCallback:   plane.dnsUpstreamReadyCallback,
+		UpstreamResolverNetwork: common.MagicNetwork("udp", global.SoMarkFromDae),
 	})
 	if err != nil {
 		return nil, err
@@ -559,7 +572,7 @@ func (c *ControlPlane) ChooseDialTarget(outbound consts.OutboundIndex, dst netip
 					// TODO: use DNS controller and re-route by control plane.
 					systemDns, err := netutils.SystemDns()
 					if err == nil {
-						if ip46, err := netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, domain, false, true); err == nil && (ip46.Ip4.IsValid() || ip46.Ip6.IsValid()) {
+						if ip46, err := netutils.ResolveIp46(ctx, direct.SymmetricDirect, systemDns, domain, common.MagicNetwork("udp", c.soMarkFromDae), true); err == nil && (ip46.Ip4.IsValid() || ip46.Ip6.IsValid()) {
 							// Has A/AAAA records. It is a real domain.
 							dialMode = consts.DialMode_Domain
 							// Add it to real-domain set.
@@ -717,8 +730,21 @@ func (c *ControlPlane) Serve(readyChan chan<- bool, listener *Listener) (err err
 					lastErr := err
 					addrHdr, dataOffset, err := ParseAddrHdr(data)
 					if err != nil {
-						c.log.Warnf("No AddrPort presented: %v, %v", lastErr, err)
-						return
+						if c.tproxyPortProtect {
+							c.log.Warnf("No AddrPort presented: %v, %v", lastErr, err)
+							return
+						} else {
+							routingResult = &bpfRoutingResult{
+								Mark:     0,
+								Must:     0,
+								Mac:      [6]uint8{},
+								Outbound: uint8(consts.OutboundControlPlaneRouting),
+								Pname:    [16]uint8{},
+								Pid:      0,
+							}
+							realDst = pktDst
+							goto destRetrieved
+						}
 					}
 					n := copy(data, data[dataOffset:])
 					data = data[:n]
@@ -731,6 +757,7 @@ func (c *ControlPlane) Serve(readyChan chan<- bool, listener *Listener) (err err
 				} else {
 					realDst = pktDst
 				}
+			destRetrieved:
 				if e := c.handlePkt(udpConn, data, common.ConvergeAddrPort(src), common.ConvergeAddrPort(pktDst), common.ConvergeAddrPort(realDst), routingResult); e != nil {
 					c.log.Warnln("handlePkt:", e)
 				}
@@ -813,6 +840,9 @@ func (c *ControlPlane) chooseBestDnsDialer(
 			outboundIndex, mark, _, err := c.Route(req.realSrc, netip.AddrPortFrom(dAddr, dnsUpstream.Port), dnsUpstream.Hostname, proto.ToL4ProtoType(), req.routingResult)
 			if err != nil {
 				return nil, err
+			}
+			if mark == 0 {
+				mark = c.soMarkFromDae
 			}
 			if int(outboundIndex) >= len(c.outbounds) {
 				return nil, fmt.Errorf("bad outbound index: %v", outboundIndex)

.github/workflows/build.yml
@@ -4,8 +4,6 @@ on:
   push:
     branches:
       - main
-      - fix*
-      - feat*
     paths:
       - "**/*.go"
       - "**/*.c"

Makefile
@@ -23,6 +23,7 @@ else
 	STRIP_FLAG := -strip=$(STRIP_PATH)
 endif
 
+# Do NOT remove the line below. This line is for CI.
 #export GOMODCACHE=$(PWD)/go-mod
 
 # Get version from .git.
@@ -41,7 +42,7 @@ dae: export GOOS=linux
 dae: ebpf
 	go build -o $(OUTPUT) -trimpath -ldflags "-s -w -X github.com/daeuniverse/dae/cmd.Version=$(VERSION) -X github.com/daeuniverse/dae/common/consts.MaxMatchSetLen_=$(MAX_MATCH_SET_LEN)" .
 
-clean-ebpf: 
+clean-ebpf:
 	@rm -f control/bpf_bpf*.go && \
 		rm -f control/bpf_bpf*.o
 fmt:

cmd/run.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"github.com/mzz2017/softwind/netproxy"
 	"github.com/mzz2017/softwind/pkg/fastrand"
+	"github.com/mzz2017/softwind/protocol/direct"
 	"net"
 	"net/http"
 	"os"
@@ -247,6 +250,20 @@ func newControlPlane(log *logrus.Logger, bpf interface{}, dnsCache map[string]*c
 	if !conf.Global.DisableWaitingNetwork && len(conf.Subscription) > 0 {
 		epo := 5 * time.Second
 		client := http.Client{
+			Transport: &http.Transport{
+				DialContext: func(ctx context.Context, network, addr string) (c net.Conn, err error) {
+					cd := netproxy.ContextDialer{Dialer: direct.SymmetricDirect}
+					conn, err := cd.DialContext(ctx, common.MagicNetwork("tcp", conf.Global.SoMarkFromDae), addr)
+					if err != nil {
+						return nil, err
+					}
+					return &netproxy.FakeNetConn{
+						Conn:  conn,
+						LAddr: nil,
+						RAddr: nil,
+					}, nil
+				},
+			},
 			Timeout: epo,
 		}
 		log.Infoln("Waiting for network...")
@@ -274,8 +291,25 @@ func newControlPlane(log *logrus.Logger, bpf interface{}, dnsCache map[string]*c
 	if len(conf.Subscription) > 0 {
 		log.Infoln("Fetching subscriptions...")
 	}
+	client := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, network, addr string) (c net.Conn, err error) {
+				cd := netproxy.ContextDialer{Dialer: direct.SymmetricDirect}
+				conn, err := cd.DialContext(ctx, common.MagicNetwork("tcp", conf.Global.SoMarkFromDae), addr)
+				if err != nil {
+					return nil, err
+				}
+				return &netproxy.FakeNetConn{
+					Conn:  conn,
+					LAddr: nil,
+					RAddr: nil,
+				}, nil
+			},
+		},
+		Timeout: 30 * time.Second,
+	}
 	for _, sub := range conf.Subscription {
-		tag, nodes, err := subscription.ResolveSubscription(log, filepath.Dir(cfgFile), string(sub))
+		tag, nodes, err := subscription.ResolveSubscription(log, &client, filepath.Dir(cfgFile), string(sub))
 		if err != nil {
 			log.Warnf(`failed to resolve subscription "%v": %v`, sub, err)
 			resolvingfailed = true

common/consts/ebpf.go
@@ -146,6 +146,7 @@ var (
 
 const (
 	TproxyMark      uint32 = 0x8000000
+	Recognize       uint16 = 0x2017
 	LoopbackIfIndex        = 1
 )
 

common/netutils/dns.go
@@ -19,7 +19,6 @@ import (
 	"github.com/mzz2017/softwind/netproxy"
 	"github.com/mzz2017/softwind/pkg/fastrand"
 	"github.com/mzz2017/softwind/pool"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/net/dns/dnsmessage"
 )
 
@@ -91,8 +90,8 @@ func SystemDns() (dns netip.AddrPort, err error) {
 	return systemDns, nil
 }
 
-func ResolveNetip(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, typ dnsmessage.Type, tcp bool) (addrs []netip.Addr, err error) {
-	resources, err := resolve(ctx, d, dns, host, typ, tcp)
+func ResolveNetip(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, typ dnsmessage.Type, network string) (addrs []netip.Addr, err error) {
+	resources, err := resolve(ctx, d, dns, host, typ, network)
 	if err != nil {
 		return nil, err
 	}
@@ -118,16 +117,14 @@ func ResolveNetip(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, ho
 	return addrs, nil
 }
 
-func ResolveNS(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, tcp bool) (records []string, err error) {
+func ResolveNS(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, network string) (records []string, err error) {
 	typ := dnsmessage.TypeNS
-	resources, err := resolve(ctx, d, dns, host, typ, tcp)
+	resources, err := resolve(ctx, d, dns, host, typ, network)
 	if err != nil {
 		return nil, err
 	}
-	logrus.Println(host, len(resources))
 	for _, ans := range resources {
 		if ans.Header.Type != typ {
-			logrus.Println(host, ans.Header.Type)
 			continue
 		}
 		ns, ok := ans.Body.(*dnsmessage.NSResource)
@@ -139,7 +136,7 @@ func ResolveNS(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host 
 	return records, nil
 }
 
-func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, typ dnsmessage.Type, tcp bool) (ans []dnsmessage.Resource, err error) {
+func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host string, typ dnsmessage.Type, network string) (ans []dnsmessage.Resource, err error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	fqdn := host
@@ -202,7 +199,11 @@ func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host st
 	if err != nil {
 		return nil, err
 	}
-	if tcp {
+	magicNetwork, err := netproxy.ParseMagicNetwork(network)
+	if err != nil {
+		return nil, err
+	}
+	if magicNetwork.Network == "tcp" {
 		// Put DNS request length
 		buf := pool.Get(2 + len(b))
 		defer pool.Put(buf)
@@ -213,12 +214,7 @@ func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host st
 
 	// Dial and write.
 	cd := &netproxy.ContextDialer{Dialer: d}
-	var c netproxy.Conn
-	if tcp {
-		c, err = cd.DialTcpContext(ctx, dns.String())
-	} else {
-		c, err = cd.DialUdpContext(ctx, dns.String())
-	}
+	c, err := cd.DialContext(ctx, network, dns.String())
 	if err != nil {
 		return nil, err
 	}
@@ -228,7 +224,7 @@ func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host st
 		return nil, err
 	}
 	ch := make(chan error, 2)
-	if !tcp {
+	if magicNetwork.Network == "udp" {
 		go func() {
 			// Resend every 3 seconds for UDP.
 			for {
@@ -249,7 +245,7 @@ func resolve(ctx context.Context, d netproxy.Dialer, dns netip.AddrPort, host st
 	go func() {
 		buf := pool.Get(512)
 		defer pool.Put(buf)
-		if tcp {
+		if magicNetwork.Network == "tcp" {
 			// Read DNS response length
 			_, err := io.ReadFull(c, buf[:2])
 			if err != nil {

common/netutils/ip46.go
@@ -22,7 +22,7 @@ type Ip46 struct {
 	Ip6 netip.Addr
 }
 
-func ResolveIp46(ctx context.Context, dialer netproxy.Dialer, dns netip.AddrPort, host string, tcp bool, race bool) (ipv46 *Ip46, err error) {
+func ResolveIp46(ctx context.Context, dialer netproxy.Dialer, dns netip.AddrPort, host string, network string, race bool) (ipv46 *Ip46, err error) {
 	var log *logrus.Logger
 	if _log := ctx.Value("logger"); _log != nil {
 		log = _log.(*logrus.Logger)
@@ -49,7 +49,7 @@ func ResolveIp46(ctx context.Context, dialer netproxy.Dialer, dns netip.AddrPort
 			}
 		}()
 		var e error
-		addrs4, e = ResolveNetip(ctx4, dialer, dns, host, dnsmessage.TypeA, tcp)
+		addrs4, e = ResolveNetip(ctx4, dialer, dns, host, dnsmessage.TypeA, network)
 		if err != nil && !errors.Is(e, context.Canceled) {
 			err4 = e
 			return
@@ -67,7 +67,7 @@ func ResolveIp46(ctx context.Context, dialer netproxy.Dialer, dns netip.AddrPort
 			}
 		}()
 		var e error
-		addrs6, e = ResolveNetip(ctx6, dialer, dns, host, dnsmessage.TypeAAAA, tcp)
+		addrs6, e = ResolveNetip(ctx6, dialer, dns, host, dnsmessage.TypeAAAA, network)
 		if err != nil && !errors.Is(e, context.Canceled) {
 			err6 = e
 			return

commit afb1f62fe9358ecbd9b40020d249a54d5d88ec60

fix: bad connectivity check of dns(udp) caused by #80

component/outbound/dialer/connectivity_check.go
@@ -10,7 +10,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/daeuniverse/dae/common"
 	"io"
 	"net"
 	"net/http"
@@ -21,6 +20,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/daeuniverse/dae/common"
 
 	"github.com/daeuniverse/dae/common/consts"
 	"github.com/daeuniverse/dae/common/netutils"
@@ -236,6 +237,7 @@ type CheckDnsOptionRaw struct {
 	mu              sync.Mutex
 	Raw             []string
 	ResolverNetwork string
+	Somark          uint32
 }
 
 func (c *CheckDnsOptionRaw) Option() (opt *CheckDnsOption, err error) {
@@ -319,6 +321,14 @@ func (d *Dialer) aliveBackground() {
 			return d.HttpCheck(ctx, opt.Url, opt.Ip6, opt.Method, tcpSomark)
 		},
 	}
+	tcpNetwork := netproxy.MagicNetwork{
+		Network: "tcp",
+		Mark:    d.CheckDnsOptionRaw.Somark,
+	}.Encode()
+	udpNetwork := netproxy.MagicNetwork{
+		Network: "udp",
+		Mark:    d.CheckDnsOptionRaw.Somark,
+	}.Encode()
 	tcp4CheckDnsOpt := &CheckOption{
 		networkType: &NetworkType{
 			L4Proto:   consts.L4ProtoStr_TCP,
@@ -338,7 +348,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), tcpNetwork)
 		},
 	}
 	tcp6CheckDnsOpt := &CheckOption{
@@ -360,7 +370,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), tcpNetwork)
 		},
 	}
 	udp4CheckDnsOpt := &CheckOption{
@@ -381,7 +391,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip4, opt.DnsPort), udpNetwork)
 		},
 	}
 	udp6CheckDnsOpt := &CheckOption{
@@ -402,7 +412,7 @@ func (d *Dialer) aliveBackground() {
 				}).Debugln("Skip check due to no DNS record.")
 				return false, nil
 			}
-			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), d.CheckDnsOptionRaw.ResolverNetwork)
+			return d.DnsCheck(ctx, netip.AddrPortFrom(opt.Ip6, opt.DnsPort), udpNetwork)
 		},
 	}
 	var CheckOpts = []*CheckOption{

control/control_plane.go
@@ -239,6 +239,7 @@ func NewControlPlane(
 		CheckDnsOptionRaw: dialer.CheckDnsOptionRaw{
 			Raw:             global.UdpCheckDns,
 			ResolverNetwork: common.MagicNetwork("udp", global.SoMarkFromDae),
+			Somark:          global.SoMarkFromDae,
 		},
 		CheckInterval:     global.CheckInterval,
 		CheckTolerance:    global.CheckTolerance,