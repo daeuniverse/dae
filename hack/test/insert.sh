d60e644e0c42265b60820b9a544557cdb4b7bd15
mzz <2017@duck.com>
2023-06-27 10:04:07 +0800

    chore/docs: support to unroll route loop and update troubleshooting.md (#158)

--- hack/test/insert.sh
+++ hack/test/insert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 lan=docker0
-wan=wlp5s0
+wan=ens192
 
 sudo tc qdisc add dev $lan clsact > /dev/null 2>&1
 sudo tc qdisc add dev $wan clsact > /dev/null 2>&1
@@ -9,8 +9,8 @@ set -ex
 
 sudo rm -rf /sys/fs/bpf/tc/globals/*
 
-# clang -fno-stack-protector -O2 -g -emit-llvm -c control/kern/tproxy.c -o - | llc -march=bpf -mcpu=v3 -mattr=+alu32 -filetype=obj -o foo.o
-clang -O2 -g -Wall -Werror -c control/kern/tproxy.c -target bpf -D__TARGET_ARCH_x86 -o foo.o
+# clang -fno-stack-protector -O2 -g -emit-llvm -c ../../control/kern/tproxy.c -o - | llc -march=bpf -mcpu=v3 -mattr=+alu32 -filetype=obj -o foo.o
+clang -O2 -g -Wall -Werror -c ../../control/kern/tproxy.c -target bpf -D__TARGET_ARCH_x86 -o foo.o
 sudo tc filter del dev $lan ingress
 sudo tc filter del dev $lan egress
 sudo tc filter del dev $wan ingress
