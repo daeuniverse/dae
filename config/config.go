8bde2f6d4518b22f12c9fa003943f5707381945b
mzz2017 <2017@duck.com>
2023-07-13 21:01:59 +0800

    fix: config marshaller

--- config/config.go
+++ config/config.go
@@ -88,7 +88,7 @@ type DnsRouting struct {
 }
 type KeyableString string
 type Dns struct {
-	IpVersionPrefer int             `mapstructure:"ipversion_prefer"`
+	IpVersionPrefer uint8           `mapstructure:"ipversion_prefer"`
 	FixedDomainTtl  []KeyableString `mapstructure:"fixed_domain_ttl"`
 	Upstream        []KeyableString `mapstructure:"upstream"`
 	Routing         DnsRouting      `mapstructure:"routing"`
