daeuniverse/dae

commit e5d3f5ba581afa2eb97ded4a096ecd3e6942325b

optimize: force to choose one if there is only one node in the group (#251)
* optimize: force to choose if there is only one node in the group

* chore: remove log

---------

Co-authored-by: dae-bot[bot] <136105375+dae-bot[bot]@users.noreply.github.com>

component/outbound/dialer_group.go
@@ -217,22 +217,31 @@ func (d *DialerGroup) MustGetAliveDialerSet(typ *dialer.NetworkType) *dialer.Ali
 
 // Select selects a dialer from group according to selectionPolicy. If 'strictIpVersion' is false and no alive dialer, it will fallback to another ipversion.
 func (g *DialerGroup) Select(networkType *dialer.NetworkType, strictIpVersion bool) (d *dialer.Dialer, latency time.Duration, err error) {
-	d, latency, err = g._select(networkType)
+	policy := g.selectionPolicy
+	d, latency, err = g._select(networkType, policy)
 	if !strictIpVersion && errors.Is(err, NoAliveDialerError) {
 		networkType.IpVersion = (consts.IpVersion_X - networkType.IpVersion.ToIpVersionType()).ToIpVersionStr()
-		return g._select(networkType)
+		return g._select(networkType, policy)
 	}
-	return d, latency, err
+	if err == nil {
+		return d, latency, nil
+	}
+	if errors.Is(err, NoAliveDialerError) && len(g.Dialers) == 1 {
+		// There is only one dialer in this group. Just choose it instead of return error.
+		return g._select(networkType, &DialerSelectionPolicy{
+			Policy:     consts.DialerSelectionPolicy_Fixed,
+			FixedIndex: 0,
+		})
+	}
+	return nil, latency, err
 }
 
-func (g *DialerGroup) _select(networkType *dialer.NetworkType) (d *dialer.Dialer, latency time.Duration, err error) {
+func (g *DialerGroup) _select(networkType *dialer.NetworkType, policy *DialerSelectionPolicy) (d *dialer.Dialer, latency time.Duration, err error) {
 	if len(g.Dialers) == 0 {
 		return nil, 0, fmt.Errorf("no dialer in this group")
 	}
-
 	a := g.MustGetAliveDialerSet(networkType)
-
-	switch g.selectionPolicy.Policy {
+	switch policy.Policy {
 	case consts.DialerSelectionPolicy_Random:
 		d := a.GetRand()
 		if d == nil {

commit 8b94e372b167e6c4c2e473875e187e7530bda748

fix: bad ipversion selection caused by #251

component/outbound/dialer/connectivity_check.go
@@ -96,6 +96,13 @@ func (d *Dialer) mustGetCollection(typ *NetworkType) *collection {
 				return d.collections[5]
 			}
 		case consts.L4ProtoStr_UDP:
+			// UDP share the DNS check result.
+			switch typ.IpVersion {
+			case consts.IpVersionStr_4:
+				return d.collections[2]
+			case consts.IpVersionStr_6:
+				return d.collections[3]
+			}
 		}
 	}
 	panic("invalid param")

component/outbound/dialer_group.go
@@ -210,6 +210,13 @@ func (d *DialerGroup) MustGetAliveDialerSet(typ *dialer.NetworkType) *dialer.Ali
 				return d.aliveDialerSets[5]
 			}
 		case consts.L4ProtoStr_UDP:
+			// UDP share the DNS check result.
+			switch typ.IpVersion {
+			case consts.IpVersionStr_4:
+				return d.aliveDialerSets[2]
+			case consts.IpVersionStr_6:
+				return d.aliveDialerSets[3]
+			}
 		}
 	}
 	panic("invalid param")
@@ -228,10 +235,13 @@ func (g *DialerGroup) Select(networkType *dialer.NetworkType, strictIpVersion bo
 	}
 	if errors.Is(err, NoAliveDialerError) && len(g.Dialers) == 1 {
 		// There is only one dialer in this group. Just choose it instead of return error.
-		return g._select(networkType, &DialerSelectionPolicy{
+		if d, _, err = g._select(networkType, &DialerSelectionPolicy{
 			Policy:     consts.DialerSelectionPolicy_Fixed,
 			FixedIndex: 0,
-		})
+		}); err != nil {
+			return nil, 0, err
+		}
+		return d, dialer.Timeout, nil
 	}
 	return nil, latency, err
 }

control/dns_control.go
@@ -543,7 +543,7 @@ func (c *DnsController) dialSend(invokingDepth int, req *udpRequest, data []byte
 	networkType := &dialer.NetworkType{
 		L4Proto:   dialArgument.l4proto,
 		IpVersion: dialArgument.ipversion,
-		IsDns:     true, // UDP relies on DNS check result.
+		IsDns:     true,
 	}
 
 	// Dial and send.

control/udp.go
@@ -175,7 +175,7 @@ func (c *ControlPlane) handlePkt(lConn *net.UDPConn, data []byte, src, pktDst, r
 	networkType := &dialer.NetworkType{
 		L4Proto:   consts.L4ProtoStr_UDP,
 		IpVersion: consts.IpVersionFromAddr(realDst.Addr()),
-		IsDns:     true, // UDP relies on DNS check result.
+		IsDns:     false,
 	}
 	// Get outbound.
 	outboundIndex := consts.OutboundIndex(routingResult.Outbound)