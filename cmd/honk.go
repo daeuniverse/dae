c784ebaf5361e055f99a32191820ee226d7e20e3
Nicolas <17946453+troubadour-hell@users.noreply.github.com>
2023-08-19 12:02:49 +0800

    feat: dae can call now (#288)

---
+++ cmd/honk.go
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * Copyright (c) 2022-2023, daeuniverse Organization <dae@v2raya.org>
+ */
+
+ package cmd
+
+ import (
+	 "fmt"
+	 "os"
+ 
+	 "github.com/spf13/cobra"
+ )
+ 
+ var (
+	 honkCmd = &cobra.Command{
+		 Use:   "honk",
+		 Short: "Let dae call for you.",
+		 Run: func(cmd *cobra.Command, args []string) {
+				 fmt.Println("Honk! Honk! Honk! This is dae!")
+				 os.Exit(0)
+		 },
+	 }
+ )
+ 
+ func init() {
+	 rootCmd.AddCommand(honkCmd)
+ }
+ 
