899d0bd8696e0bd0970103b69145175aee73b314
mzz2017 <2017@duck.com>
2023-08-27 18:15:33 +0800

    optimize(udp): support gso

--- control/anyfrom_pool.go
+++ control/anyfrom_pool.go
@@ -2,23 +2,38 @@ package control
 
 import (
 	"context"
+	"errors"
+	"math"
 	"net"
 	"net/netip"
+	"os"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
+	"unsafe"
 
 	"github.com/daeuniverse/dae/component/outbound/dialer"
+	"golang.org/x/sys/unix"
 )
 
 type Anyfrom struct {
 	*net.UDPConn
 	deadlineTimer *time.Timer
 	ttl           time.Duration
+	// GSO support is modified from quic-go with many thanks.
+	gso         bool
+	gotGSOError bool
 }
 
 func (a *Anyfrom) RefreshTtl() {
 	a.deadlineTimer.Reset(a.ttl)
+}
+func (a *Anyfrom) SupportGso(size int) bool {
+	if size > math.MaxUint16 {
+		return false
+	}
+	return a.gso && a.gotGSOError
 }
 func (a *Anyfrom) ReadFrom(b []byte) (int, net.Addr, error) {
 	defer a.RefreshTtl()
@@ -43,31 +58,114 @@ func (a *Anyfrom) ReadMsgUDPAddrPort(b []byte, oob []byte) (n int, oobn int, fla
 func (a *Anyfrom) SyscallConn() (syscall.RawConn, error) {
 	defer a.RefreshTtl()
 	return a.UDPConn.SyscallConn()
-
 }
 func (a *Anyfrom) WriteMsgUDP(b []byte, oob []byte, addr *net.UDPAddr) (n int, oobn int, err error) {
-	defer a.RefreshTtl()
+	defer func() {
+		if isGSOError(err) {
+			a.gotGSOError = true
+		}
+		a.RefreshTtl()
+	}()
+	if a.SupportGso(len(b)) {
+		return a.UDPConn.WriteMsgUDP(b, appendUDPSegmentSizeMsg(oob, uint16(len(b))), addr)
+	}
 	return a.UDPConn.WriteMsgUDP(b, oob, addr)
-
 }
 func (a *Anyfrom) WriteMsgUDPAddrPort(b []byte, oob []byte, addr netip.AddrPort) (n int, oobn int, err error) {
-	defer a.RefreshTtl()
+	defer func() {
+		if isGSOError(err) {
+			a.gotGSOError = true
+		}
+		a.RefreshTtl()
+	}()
+	if a.SupportGso(len(b)) {
+		return a.UDPConn.WriteMsgUDPAddrPort(b, appendUDPSegmentSizeMsg(oob, uint16(len(b))), addr)
+	}
 	return a.UDPConn.WriteMsgUDPAddrPort(b, oob, addr)
-
 }
-func (a *Anyfrom) WriteTo(b []byte, addr net.Addr) (int, error) {
-	defer a.RefreshTtl()
+func (a *Anyfrom) WriteTo(b []byte, addr net.Addr) (n int, err error) {
+	defer func() {
+		if isGSOError(err) {
+			a.gotGSOError = true
+		}
+		a.RefreshTtl()
+	}()
+	if a.SupportGso(len(b)) {
+		n, _, err = a.UDPConn.WriteMsgUDP(b, appendUDPSegmentSizeMsg(nil, uint16(len(b))), addr.(*net.UDPAddr))
+		return n, err
+	}
 	return a.UDPConn.WriteTo(b, addr)
-
 }
-func (a *Anyfrom) WriteToUDP(b []byte, addr *net.UDPAddr) (int, error) {
-	defer a.RefreshTtl()
+func (a *Anyfrom) WriteToUDP(b []byte, addr *net.UDPAddr) (n int, err error) {
+	defer func() {
+		if isGSOError(err) {
+			a.gotGSOError = true
+		}
+		a.RefreshTtl()
+	}()
+	if a.SupportGso(len(b)) {
+		n, _, err = a.UDPConn.WriteMsgUDP(b, appendUDPSegmentSizeMsg(nil, uint16(len(b))), addr)
+		return n, err
+	}
 	return a.UDPConn.WriteToUDP(b, addr)
-
 }
-func (a *Anyfrom) WriteToUDPAddrPort(b []byte, addr netip.AddrPort) (int, error) {
-	defer a.RefreshTtl()
+func (a *Anyfrom) WriteToUDPAddrPort(b []byte, addr netip.AddrPort) (n int, err error) {
+	defer func() {
+		if isGSOError(err) {
+			a.gotGSOError = true
+		}
+		a.RefreshTtl()
+	}()
+	if a.SupportGso(len(b)) {
+		n, _, err = a.UDPConn.WriteMsgUDPAddrPort(b, appendUDPSegmentSizeMsg(nil, uint16(len(b))), addr)
+		return n, err
+	}
 	return a.UDPConn.WriteToUDPAddrPort(b, addr)
+}
+
+// isGSOSupported tests if the kernel supports GSO.
+// Sending with GSO might still fail later on, if the interface doesn't support it (see isGSOError).
+func isGSOSupported(uc *net.UDPConn) bool {
+	conn, err := uc.SyscallConn()
+	if err != nil {
+		return false
+	}
+	disabled, err := strconv.ParseBool(os.Getenv("DAE_DISABLE_GSO"))
+	if err == nil && disabled {
+		return false
+	}
+	var serr error
+	if err := conn.Control(func(fd uintptr) {
+		_, serr = unix.GetsockoptInt(int(fd), unix.IPPROTO_UDP, unix.UDP_SEGMENT)
+	}); err != nil {
+		return false
+	}
+	return serr == nil
+}
+func isGSOError(err error) bool {
+	var serr *os.SyscallError
+	if errors.As(err, &serr) {
+		// EIO is returned by udp_send_skb() if the device driver does not have tx checksums enabled,
+		// which is a hard requirement of UDP_SEGMENT. See:
+		// https://git.kernel.org/pub/scm/docs/man-pages/man-pages.git/tree/man7/udp.7?id=806eabd74910447f21005160e90957bde4db0183#n228
+		// https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/net/ipv4/udp.c?h=v6.2&id=c9c3395d5e3dcc6daee66c6908354d47bf98cb0c#n942
+		return serr.Err == unix.EIO
+	}
+	return false
+}
+func appendUDPSegmentSizeMsg(b []byte, size uint16) []byte {
+	startLen := len(b)
+	const dataLen = 2 // payload is a uint16
+	b = append(b, make([]byte, unix.CmsgSpace(dataLen))...)
+	h := (*unix.Cmsghdr)(unsafe.Pointer(&b[startLen]))
+	h.Level = syscall.IPPROTO_UDP
+	h.Type = unix.UDP_SEGMENT
+	h.SetLen(unix.CmsgLen(dataLen))
+
+	// UnixRights uses the private `data` method, but I *think* this achieves the same goal.
+	offset := startLen + unix.CmsgSpace(0)
+	*(*uint16)(unsafe.Pointer(&b[offset])) = size
+	return b
 }
 
 // AnyfromPool is a full-cone udp listener pool
@@ -107,10 +205,13 @@ func (p *AnyfromPool) GetOrCreate(lAddr string, ttl time.Duration) (conn *Anyfro
 		if err != nil {
 			return nil, true, err
 		}
+		uConn := pc.(*net.UDPConn)
 		af = &Anyfrom{
-			UDPConn:       pc.(*net.UDPConn),
+			UDPConn:       uConn,
 			deadlineTimer: nil,
 			ttl:           ttl,
+			gotGSOError:   false,
+			gso:           isGSOSupported(uConn),
 		}
 		af.deadlineTimer = time.AfterFunc(ttl, func() {
 			p.mu.Lock()
