5b3c0fe7b1785f97d39b764d6c1c4c2ab7dd03b2
LuoChen <luochen1990@gmail.com>
2023-09-02 17:45:04 +0800

    improve: latencyString implementation

--- component/outbound/dialer/utils.go
+++ component/outbound/dialer/utils.go
@@ -3,15 +3,19 @@ package dialer
 import "time"
 
 func latencyString(realLatency, latencyOffset time.Duration) string {
-	offsetPart := func() string {
-		if latencyOffset > 0 {
-			return "(+" + latencyOffset.Truncate(time.Millisecond).String() + ")"
-		} else if latencyOffset < 0 {
-			return "(" + latencyOffset.Truncate(time.Millisecond).String() + ")"
-		} else { // latencyOffset == 0
-			return ""
-		}
-	}()
+	var offsetSign string
+	if latencyOffset > 0 {
+		offsetSign = "+"
+	} else {
+		offsetSign = "-"
+	}
+
+	var offsetPart string
+	if latencyOffset != 0 {
+		offsetPart = "(" + offsetSign + latencyOffset.Truncate(time.Millisecond).Abs().String() + ")"
+	} else { // latencyOffset == 0
+		offsetPart = ""
+	}
 
 	return realLatency.Truncate(time.Millisecond).String() + offsetPart
 }
