798546899b8bae02dde24e94ca4ddfe4e566545b
sumire <sumire88@proton.me>
2024-01-03 21:05:25 +0800

    chore(hack/maintenance): add script to automatically insert license comments

---
+++ hack/maintenance/append_license_signature.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Command to obtain the list of target files
+files=$(rg -F 'SPDX-License-Identifier: AGPL-3.0-only' --files-without-match --glob '*.go' --glob '!pkg/ebpf_internal/**/*.go' --glob '!pkg/geodata/**/*.go' .)
+
+# Insert the specified lines to the top of each target file
+insert_lines() {
+  local file="$1"
+  if [ -f "$file" ]; then
+    # Inserting lines at the beginning of the file
+    {
+      echo "#"
+      echo "#  SPDX-License-Identifier: AGPL-3.0-only"
+      echo "#  Copyright (c) 2022-2024, daeuniverse Organization <dae@v2raya.org>"
+      echo "#"
+      echo
+      cat "$file"
+    } >tempfile && mv tempfile "$file"
+    echo "Lines inserted into $file"
+  else
+    echo "File not found: $file"
+  fi
+}
+
+# Loop through each file and insert lines to the top
+while IFS= read -r file; do
+  insert_lines "$file"
+done <<<"$files"
