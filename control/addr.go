883437b6a3e6c66d18137b9520494dedcfc52232
/gray <greyschwinger@gmail.com>
2024-01-23 20:50:07 +0800

    refactor/optimize: remove dead code (#411)

--- control/addr.go
+++ control/addr.go
@@ -9,12 +9,10 @@ import (
 	"net"
 	"net/netip"
 	"strconv"
-
-	"github.com/daeuniverse/dae/common/consts"
 )
 
-func RefineSourceToShow(src netip.AddrPort, dst netip.Addr, lanWanFlag consts.LanWanFlag) (srcToShow string) {
-	if lanWanFlag == consts.LanWanFlag_IsWan || src.Addr() == dst {
+func RefineSourceToShow(src netip.AddrPort, dst netip.Addr) (srcToShow string) {
+	if src.Addr() == dst {
 		// If nothing else, this means this packet is sent from localhost.
 		return net.JoinHostPort("localhost", strconv.Itoa(int(src.Port())))
 	} else {
