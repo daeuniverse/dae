e2dd918851c1f3a34495d3d6e29ebe8ee5f35cb1
ShengHuang <shenghuang147@gmail.com>
2024-06-26 14:21:54 +0800

    chore: Fromat the file

--- install/shell-completion/dae.bash
+++ install/shell-completion/dae.bash
@@ -3,7 +3,7 @@
 # To be installed in "/usr/share/bash-completion/completions/dae"
 
 _dae() {
-	local prev cur cmd export_cmd run_opts validate_opts
+	local prev cur cmd export_cmd run_opts trace_opts validate_opts
 	COMPREPLY=()
 
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -11,23 +11,24 @@ _dae() {
 	cmd="export help honk reload run suspend trace validate"
 	export_cmd="outline"
 	run_opts="-c --config --disable-pidfile --disable-timestamp --logfile \
-        --logfile-maxbackups --logfile-maxsize"
-    trace_opts="-4 --ipv4 -6 --ipv6 -p --l4-proto -o --output -P \
-        --port"
-    validate_opts="-c --config"
+		--logfile-maxbackups --logfile-maxsize"
+	trace_opts="-4 --ipv4 -6 --ipv6 -p --l4-proto -o --output -P \
+		--port"
+	validate_opts="-c --config"
 
 	case "${prev}" in
 		help)
 			return 0
 			;;
 
-        honk|reload|suspend|outline)
+		honk|reload|suspend|outline)
 			COMPREPLY=( $(compgen -W "-h --help" -- "${cur}") )
-            return 0
-            ;;
+			return 0
+			;;
             
 		export)
-			COMPREPLY=( $(compgen -W "$export_cmd -h --help" -- "${cur}") )
+			COMPREPLY=( $(compgen -W "$export_cmd -h --help" -- \
+				"${cur}") )
 			return 0
 			;;
             
@@ -48,59 +49,64 @@ _dae() {
 				"${cur}") )
 			return 0
 			;;
-        
-        # multiple option matching
-		--disable-pidfile|--disable-timestamp|--logfile|--logfile-maxbackup|\
-        --logfile-maxsize|-c|--config)
 
-            case "${prev}" in
-                --logfile)
-                    COMPREPLY=()
-                    _filedir '@(log)'
-                    return 0
-                    ;;
-            esac
+		# multiple option matching
+		--disable-pidfile|--disable-timestamp|--logfile|\
+			--logfile-maxbackup|--logfile-maxsize|-c|--config)
 
-            case "${prev}" in
-                -c|--config)
-                    COMPREPLY=()
-                    _filedir '@(dae|d)'
-                    return 0
-                    ;;
-            esac
+			case "${prev}" in
+				--logfile)
+					COMPREPLY=()
+					_filedir '@(log)'
+					return 0
+					;;
+			esac
 
-            case "${COMP_WORDS[1]}" in
-                run)
-                    COMPREPLY=( $(compgen -W "$run_opts" -- "${cur}") )
-                    return 0
-                    ;;
-            esac
+			case "${prev}" in
+				-c|--config)
+					COMPREPLY=()
+					_filedir '@(dae|d)'
+					return 0
+					;;
+			esac
 
-            return 0
-            ;;
+			case "${COMP_WORDS[1]}" in
+				run)
+					COMPREPLY=( $(compgen -W "$run_opts" -- \
+						"${cur}") )
+					return 0
+					;;
+			esac
+
+			return 0
+			;;
 
 		-h|--help)
 			return 0
 			;;
+
 		*)
-		;;
+			;;
 	esac
 
 	case "${cur}" in
 		-*)
-			COMPREPLY=( $( compgen -W "--version --help -v -h" -- "${cur}") )
+			COMPREPLY=( $( compgen -W "--version --help -v -h" -- \
+				"${cur}") )
 			return 0
 			;;
 		--*)
-			COMPREPLY=( $( compgen -W "--version --help" -- "${cur}") )
+			COMPREPLY=( $( compgen -W "--version --help" -- \
+				"${cur}") )
 			return 0
 			;;
 		*)
-            case "${COMP_WORDS[1]}" in
-                export|help|honk|reload|run|suspend|validate)
-                    return 0
-                    ;;
-            esac
+			case "${COMP_WORDS[1]}" in
+				export|help|honk|reload|run|suspend|validate)
+					return 0
+					;;
+			esac
+
 			COMPREPLY=( $( compgen -W "${cmd}" -- "${cur}") )
 			return 0
 			;;
