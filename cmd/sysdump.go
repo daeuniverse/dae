6c76249fe7bf4df3bc0612227bc0065ca5869af8
linglilongyi <linglilongyi@gmail.com>
2024-07-03 22:42:08 +0800

    patch-1

--- cmd/sysdump.go
+++ cmd/sysdump.go
@@ -13,11 +13,13 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
-    
+    "time"
+
 	"github.com/vishvananda/netlink"
 	"github.com/spf13/cobra"
 	"github.com/mholt/archiver/v3"
 	"github.com/shirou/gopsutil/v4/net"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -44,7 +46,7 @@ func dumpNetworkInfo() {
 	dumpNetfilter(tempDir)
 	dumpIPTables(tempDir)
 
-	tarFile := "sysdump.tar.gz"
+	tarFile := fmt.Sprintf("sysdump.%d.tar.gz",time.Now().Unix())
 	if err := archiver.Archive([]string{tempDir}, tarFile); err != nil {
 		fmt.Printf("Failed to create tar archive: %v\n", err)
 		return
@@ -52,6 +54,109 @@ func dumpNetworkInfo() {
 
 	fmt.Printf("System network information collected and saved to %s\n", tarFile)
 }
+
+
+// Translate scope enum into semantic words
+func scopeToString(scope netlink.Scope) string {
+	switch scope {
+	case unix.RT_SCOPE_UNIVERSE:
+		return "universe"
+	case unix.RT_SCOPE_SITE:
+		return "site"
+	case unix.RT_SCOPE_LINK:
+		return "link"
+	case unix.RT_SCOPE_HOST:
+		return "host"
+	case unix.RT_SCOPE_NOWHERE:
+		return "nowhere"
+	default:
+		return "unknown"
+	}
+}
+
+
+// Translate protocol enum into semantic words
+func protocolToString(proto int) string {
+	switch proto {
+	case unix.RTPROT_BABEL:
+		return "babel"
+	case unix.RTPROT_BGP:
+		return "bgp"
+	case unix.RTPROT_BIRD:
+		return "bird"
+	case unix.RTPROT_BOOT:
+		return "boot"
+	case unix.RTPROT_DHCP:
+		return "dhcp"
+	case unix.RTPROT_DNROUTED:
+		return "dnrouted"
+	case unix.RTPROT_EIGRP:
+		return "eigrp"
+	case unix.RTPROT_GATED:
+		return "gated"
+	case unix.RTPROT_ISIS:
+		return "isis"
+	case unix.RTPROT_KERNEL:
+		return "kernel"
+	case unix.RTPROT_MROUTED:
+		return "mrouted"
+	case unix.RTPROT_MRT:
+		return "mrt"
+	case unix.RTPROT_NTK:
+		return "ntk"
+	case unix.RTPROT_OSPF:
+		return "ospf"
+	case unix.RTPROT_RA:
+		return "ra"
+	case unix.RTPROT_REDIRECT:
+		return "redirect"
+	case unix.RTPROT_RIP:
+		return "rip"
+	case unix.RTPROT_STATIC:
+		return "static"
+	case unix.RTPROT_UNSPEC:
+		return "unspec"
+	case unix.RTPROT_XORP:
+		return "xorp"
+	case unix.RTPROT_ZEBRA:
+		return "zebra"
+	default:
+		return "unknown"
+	}
+}
+
+// Translate route.type enum into semantic words
+func typeToString(typ int) string {
+	switch typ {
+	case unix.RTN_UNSPEC:
+		return "unspec"
+	case unix.RTN_UNICAST:
+		return "unicast"
+	case unix.RTN_LOCAL:
+		return "local"
+	case unix.RTN_BROADCAST:
+		return "broadcast"
+	case unix.RTN_ANYCAST:
+		return "anycast"
+	case unix.RTN_MULTICAST:
+		return "multicast"
+	case unix.RTN_BLACKHOLE:
+		return "blackhole"
+	case unix.RTN_UNREACHABLE:
+		return "unreachable"
+	case unix.RTN_PROHIBIT:
+		return "prohibit"
+	case unix.RTN_THROW:
+		return "throw"
+	case unix.RTN_NAT:
+		return "nat"
+	case unix.RTN_XRESOLVE:
+		return "xresolve"
+	default:
+		return "unknown"
+	}
+}
+
 
 func dumpRouting(outputDir string) {
 	routes, err := netlink.RouteList(nil, netlink.FAMILY_ALL)
@@ -84,15 +189,15 @@ func dumpRouting(outputDir string) {
 		routeStr += fmt.Sprintf(" dev %s", ifaceName)
 
 		if route.Scope != 0 {
-			routeStr += fmt.Sprintf(" scope %d", route.Scope)
+			routeStr += fmt.Sprintf(" scope %d", scopeToString(route.Scope))
 		}
 
 		if route.Protocol != 0 {
-			routeStr += fmt.Sprintf(" proto %d", route.Protocol)
+			routeStr += fmt.Sprintf(" proto %d", protocolToString(route.Protocol))
 		}
 
 		if route.Type != 0 {
-			routeStr += fmt.Sprintf(" type %d", route.Type)
+			routeStr += fmt.Sprintf(" type %d", typeToString(route.Type))
 		}
 
 		if route.Flags != 0 {
@@ -134,13 +239,13 @@ func dumpSysctl(outputDir string) {
 
 	err := filepath.Walk(sysctlPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			fmt.Printf("Fail in filepath.Walk: %v\n", err)
 		}
 
 		if !info.IsDir() {
 			value, err := ioutil.ReadFile(path)
 			if err != nil {
-				return err
+				fmt.Printf("Fail in filepath.Walk: %v\n", err)
 			}
 
 			relativePath := strings.TrimPrefix(path, sysctlPath+"/")
@@ -168,14 +273,21 @@ func dumpNetfilter(outputDir string) {
 }
 
 func dumpIPTables(outputDir string) {
-	cmd := exec.Command("iptables-save", "-t", "nat")
-	output, err := cmd.CombinedOutput()
+	iptables := exec.Command("iptables-save", "-c")
+	output, err := iptables.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Failed to get iptables NAT table: %v\n", err)
-		return
+		fmt.Printf("Failed to get iptables: %v\n", err)
+	} else {
+		ioutil.WriteFile(filepath.Join(outputDir, "iptables.txt"), output, 0644)
 	}
 
-	ioutil.WriteFile(filepath.Join(outputDir, "iptables_nat.txt"), output, 0644)
+	ip6tables := exec.Command("ip6tables-save","-c")
+	output, err = ip6tables.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to get ip6tables: %v\n", err)
+	} else {
+    ioutil.WriteFile(filepath.Join(outputDir, "ip6tables.txt"), output, 0644)
+	}
 }
 
 func init() {
