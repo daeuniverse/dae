07e2b1d7dddd6bd1c7b2436d80a91e0d338e7913
zzzsyyy <me@zzzsy.top>
2024-07-10 18:40:02 +0800

    feat: shell completion subcommand

---
+++ cmd/completion.go
@@ -0,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * Copyright (c) 2022-2024, daeuniverse Organization <dae@v2raya.org>
+ */
+
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	completionCmd = &cobra.Command{
+		Use:       "completion [bash|zsh|fish]",
+		Short:     "Output shell completion code for the specified shell (bash, zsh or fish)",
+		Long:      "Output shell completion code for the specified shell (bash, zsh or fish).",
+		Args:      cobra.ExactArgs(1),
+		ValidArgs: []string{"bash", "zsh", "fish"},
+		Hidden:    true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			out, err := getCompletion(args[0], cmd.Parent())
+			if err != nil {
+				return err
+			}
+
+			fmt.Print(out)
+			return nil
+		},
+	}
+)
+
+// return the auto completion shell script, if supported
+func getCompletion(sh string, parent *cobra.Command) (string, error) {
+	var err error
+	var buf bytes.Buffer
+
+	switch sh {
+	case "bash":
+		err = parent.GenBashCompletion(&buf)
+	case "zsh":
+		err = parent.GenZshCompletion(&buf)
+	case "fish":
+		err = parent.GenFishCompletion(&buf, true)
+	default:
+		err = errors.New("unsupported shell type (must be bash, zsh or fish): " + sh)
+	}
+
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
