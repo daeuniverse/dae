7f5fbddf446e100953d46c98460c33223f39c265
MaurUppi <myway.cn@gmail.com>
2026-02-23 14:47:17 +0800

    test(metrics): verify all collector descriptors are exposed

--- pkg/metrics/collector_describe_test.go
+++ pkg/metrics/collector_describe_test.go
@@ -36,28 +36,54 @@ func requireDescriptor(t *testing.T, names map[string]struct{}, name string) {
 	}
 }
 
+func requireDescriptors(t *testing.T, names map[string]struct{}, expected []string) {
+	t.Helper()
+	for _, name := range expected {
+		requireDescriptor(t, names, name)
+	}
+}
+
 func TestDnsCollectorDescribeIncludesPhase2Descriptors(t *testing.T) {
 	names := descriptorNames(NewDnsCollector(nil))
-	requireDescriptor(t, names, "dae_dns_query_total")
-	requireDescriptor(t, names, "dae_dns_cache_hit_total")
-	requireDescriptor(t, names, "dae_dns_cache_lazy_hit_total")
-	requireDescriptor(t, names, "dae_dns_cache_miss_total")
-	requireDescriptor(t, names, "dae_dns_upstream_query_total")
-	requireDescriptor(t, names, "dae_dns_upstream_err_total")
-	requireDescriptor(t, names, "dae_dns_rejected_total")
-	requireDescriptor(t, names, "dae_dns_refused_total")
-	requireDescriptor(t, names, "dae_dns_response_latency_seconds")
-	requireDescriptor(t, names, "dae_dns_upstream_latency_seconds")
+	requireDescriptors(t, names, []string{
+		"dae_dns_cache_entries",
+		"dae_dns_concurrency_in_use",
+		"dae_dns_concurrency_limit",
+		"dae_dns_forwarder_cache_entries",
+		"dae_dns_forwarder_in_flight",
+		"dae_dns_query_total",
+		"dae_dns_cache_hit_total",
+		"dae_dns_cache_lazy_hit_total",
+		"dae_dns_cache_miss_total",
+		"dae_dns_upstream_query_total",
+		"dae_dns_upstream_err_total",
+		"dae_dns_rejected_total",
+		"dae_dns_refused_total",
+		"dae_dns_response_latency_seconds",
+		"dae_dns_upstream_latency_seconds",
+	})
 }
 
 func TestDialerCollectorDescribeIncludesPhase2Descriptors(t *testing.T) {
 	names := descriptorNames(NewDialerCollector(nil))
-	requireDescriptor(t, names, "dae_health_check_total")
-	requireDescriptor(t, names, "dae_health_check_failure_total")
+	requireDescriptors(t, names, []string{
+		"dae_dialer_alive",
+		"dae_dialer_latency_last_seconds",
+		"dae_dialer_latency_avg10_seconds",
+		"dae_dialer_latency_moving_avg_seconds",
+		"dae_health_check_total",
+		"dae_health_check_failure_total",
+		"dae_group_alive_dialers_total",
+	})
 }
 
 func TestConnCollectorDescribeIncludesPhase2Descriptors(t *testing.T) {
 	names := descriptorNames(NewConnCollector(nil))
-	requireDescriptor(t, names, "dae_tcp_connections_total")
-	requireDescriptor(t, names, "dae_udp_connections_total")
+	requireDescriptors(t, names, []string{
+		"dae_tcp_connections_active",
+		"dae_udp_endpoints_active",
+		"dae_udp_task_queues_active",
+		"dae_tcp_connections_total",
+		"dae_udp_connections_total",
+	})
 }
