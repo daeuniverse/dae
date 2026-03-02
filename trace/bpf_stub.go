04eaeb7c3695df313fa9b8f3bc01fd1f0f09da3e
kix <olices@9up.in>
2026-03-03 03:04:05 +0800

    refactor: replace sleep with dynamic wait for eBPF program loading and add bpf_stub.go for error handling

---
+++ trace/bpf_stub.go
@@ -0,0 +1,34 @@
+//go:build !dae_real_ebpf
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * Copyright (c) 2022-2025, daeuniverse Organization <dae@v2raya.org>
+ */
+
+package trace
+
+import (
+	"errors"
+
+	"github.com/cilium/ebpf"
+)
+
+var errBpfObjectsUnavailable = errors.New("eBPF objects are unavailable in this build; run make ebpf and build with -tags dae_real_ebpf")
+
+type bpfObjects struct {
+	KprobeSkb1                   *ebpf.Program
+	KprobeSkb2                   *ebpf.Program
+	KprobeSkb3                   *ebpf.Program
+	KprobeSkb4                   *ebpf.Program
+	KprobeSkb5                   *ebpf.Program
+	KprobeSkbLifetimeTermination *ebpf.Program
+	Events                       *ebpf.Map
+}
+
+func (o *bpfObjects) Close() error {
+	return nil
+}
+
+func loadBpf() (*ebpf.CollectionSpec, error) {
+	return nil, errBpfObjectsUnavailable
+}
