db3563874be73ae3fda552f90d6417f1503505cc
LiYang <liyang@LiYangdeMac-mini.local>
2026-06-13 18:49:22 +0800

    fix: brace indentation in FixedWithFallback block
    The closing brace for case FixedWithFallback and the preceding
    return statement had incorrect indentation, causing the compiler
    to misinterpret the block structure.

--- component/outbound/dialer_group.go
+++ component/outbound/dialer_group.go
@@ -527,7 +527,7 @@ func (g *DialerGroup) _select(networkType *dialer.NetworkType, state *dialerGrou
 			}
 		}
 		return nil, time.Hour, nil, ErrNoAliveDialer
-		}
+	}
 
 	case consts.DialerSelectionPolicy_MinLastLatency,
 		consts.DialerSelectionPolicy_MinAverage10Latencies,
